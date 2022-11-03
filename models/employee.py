af051268ea5007ff85389bf64cae5630363b830d
Satyam Naithani <satyam.naithani@niyotail.com>
2022-11-04 02:09:23 +0530

    base-setup

--- models/employee.py
+++ models/employee.py
@@ -8,7 +8,7 @@ class Employee(declarative_base()):
     id: Unicode = Column(BigInteger, primary_key=True, nullable=False)
     employee_code = Column(Unicode(255))
     name = Column(Unicode(255))
-    department_id = Column(BigInteger, ForeignKey("departments.id"))
+    department_id = Column(BigInteger)
     mobile = Column(Unicode(255))
     email = Column(Unicode(255))
     password = Column(Unicode(255))
