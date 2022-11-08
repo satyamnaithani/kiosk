3fc1e2d65c1a1767cbdbdfbf21a09dfdf3dbf5af
Satyam Naithani <satyam.naithani@niyotail.com>
2022-11-08 22:36:09 +0530

    changed-db

--- config/db.py
+++ config/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 def get_session() -> Session:
-    db_connection = "mysql://satyam:kljkhkBckndlabcinHecd3ZTpb@35.238.57.231:3306/kiosk"
+    db_connection = "mysql+pymysql://satyam:kljkhkBckndlabcinHecd3ZTpb@35.238.57.231:3306/kiosk"
     engine = create_engine(
         db_connection, echo=True, connect_args={"connect_timeout": 10}
     )
