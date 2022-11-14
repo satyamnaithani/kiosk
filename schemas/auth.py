c251064dd180a4d75a1649a833c287e055446548
Satyam Naithani <satyam.naithani@niyotail.com>
2022-11-14 21:57:39 +0530

    swagger

--- schemas/auth.py
+++ schemas/auth.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 class LoginSchema(BaseModel):
     email: str
     password: str
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "satyam@gmail.com",
+                "password": "hell"
+            }
+        }
